7277ef31c42c46291c8aeee2c4bf99f2a9ca6ba4
NASEEM A P <naseemalassampattil@gmail.com>
2024-10-10 13:18:28 +0530

    updated model links

--- download_model.sh
+++ download_model.sh
@@ -6,11 +6,13 @@ pip install gdown==4.6.1
 # Download LPRNet model
 mkdir -p ./models/tao_pretrained_models/lprnet
 cd ./models/tao_pretrained_models/lprnet
-gdown 1lmu7aSZfNWQnLFeE6UnlAouYGao8842b
+gdown 1ue_M-uNPzniabCo48wMbLpJNG9LiRpxu
+gdown 1SFQSSTecLnmFNeDXDdBmKLxRmdIKP9Dt
 cd -
 
 # Download LPRNet model
 mkdir -p ./models/tao_pretrained_models/yolov8-lpd
 cd ./models/tao_pretrained_models/yolov8-lpd
-gdown 1fC8Qd6LC38UtBNwZV4KsQJfCdjhu0rHN
+gdown 1dM-vITKR9uEoyvxPzbqWsNONuQA2shNk
+gdown 1eA4j_YJeAiPZgPhC9V3XSOm9JzmB_TQT
 cd -
